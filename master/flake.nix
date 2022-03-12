37d1b4c8a6164873af1d5de5d177124bd0ec7a8b
hugosenari <hugosenari@gmail.com>
2022-03-12 00:49:17 -0300

    chore: re index bisect

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Bisection algorithms ported from Python'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-bisect-master.flake = false;
+  inputs.src-bisect-master.owner = "berquist";
+  inputs.src-bisect-master.ref   = "refs/heads/master";
+  inputs.src-bisect-master.repo  = "bisect";
+  inputs.src-bisect-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-bisect-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-bisect-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
