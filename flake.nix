f55f4fcf32d84363fb684a1fa7ed9f9158e837c1
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:38 -0300

    chore: re index bisect

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bisect-master".dir   = "master";
-  inputs."bisect-master".owner = "nim-nix-pkgs";
-  inputs."bisect-master".ref   = "master";
-  inputs."bisect-master".repo  = "bisect";
-  inputs."bisect-master".type  = "github";
-  inputs."bisect-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bisect-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."bisect-v0_1_1".dir   = "v0_1_1";
-  inputs."bisect-v0_1_1".owner = "nim-nix-pkgs";
-  inputs."bisect-v0_1_1".ref   = "master";
-  inputs."bisect-v0_1_1".repo  = "bisect";
-  inputs."bisect-v0_1_1".type  = "github";
-  inputs."bisect-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bisect-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
